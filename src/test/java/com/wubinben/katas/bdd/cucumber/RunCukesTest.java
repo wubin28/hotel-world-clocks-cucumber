50d401af7f779b81cf65676b6494ee95d2e4a54a
Ben Wu <wubin28@gmail.com>
2017-12-04 13:45:19 +0800

    add cucumber feature files and RunCukesTest so that we can have the missing steps after running 'mvn clean test'

---
+++ src/test/java/com/wubinben/katas/bdd/cucumber/RunCukesTest.java
@@ -0,0 +1,9 @@
+package com.wubinben.katas.bdd.cucumber;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"})
+public class RunCukesTest {
+}
